19581ebee1e411ea74bfe7df9d3a68e22c0a872c
CryoCardiogram <menalewa@live.com>
2018-12-09 19:22:39 +0100

    clean unused files

--- src/test.py
+++
@@ -1,9 +0,0 @@
-import sys
-from getopt import gnu_getopt
-
-if __name__ == '__main__':
-    options = 'hln:s:'
-    opt_arg, args =  gnu_getopt(sys.argv[1:], options)
-    print(opt_arg)
-    for opt, arg in opt_arg:
-        print('option {}: {}'.format(opt, arg))
